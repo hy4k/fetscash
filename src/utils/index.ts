fa5218fc08e19abf5c4f0b294522eafd2d198f7a
hy4k <mithun@fets.in>
2026-06-22 10:56:49 +0530

    Fix barrel export aliases to match actual source function names

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
-export { generateInvoicePdf } from './invoicePdf';
+export { generateInvoicePDF as generateInvoicePdf } from './invoicePdf';
 export { numberToWords } from './numberToWords';
-export { getPaybookMonthData } from './paybookMonth';
-export { generatePayslipPdf } from './paybookPayslip';
-export { generateRemittancePdf } from './remittancePdf';
-export { checkSchema } from './schemaCheck';
+export { canonicalMonthFromDate as getPaybookMonthData } from './paybookMonth';
+export { downloadPayslipPdf as generatePayslipPdf, downloadAllPayslipsPdf, downloadPayrollRegisterPdf } from './paybookPayslip';
+export { generateRemittancePDF as generateRemittancePdf, downloadRemittancePDF } from './remittancePdf';
+export { isMigration002Applied as checkSchema, isMigration003Applied, clearSchemaCache } from './schemaCheck';
 export { formatCurrency, formatDate, formatMonthLabel } from './formatters';
 export { generateNextId } from './idGenerators';
 export { resolveWorkspaceUserId } from './workspaceUser';
