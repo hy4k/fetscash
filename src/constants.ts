e8a2b2f7a2261e886ad178c1d3b207883deabe7d
hy4k <mithun@fets.in>
2026-06-22 09:51:18 +0530

    utils: add constants.ts with company info

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const CATEGORY_REPLENISHMENT = 'Replenishment';
+export const COMPANY_INFO = {
+  name: 'Forum Testing & Educational Services',
+  gstNumber: '32AAIFF5955B1ZO',
+  address: 'Cochin, Kerala, India',
+  phone: '',
+  email: '',
+};
