a75ba194902d3f21288c5854d717befab58a9c0b
hy4k <mithun@fets.in>
2026-06-22 09:46:19 +0530

    types: add user.ts type definitions

---
+++ src/types/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: string;
+  email?: string;
+  company_name?: string;
+  gst_number?: string;
+  pan_number?: string;
+  address?: string;
+  phone?: string;
+  logo_url?: string;
+}
